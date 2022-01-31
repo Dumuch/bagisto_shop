05fa5691e2817e073d7cdc791251fea64ed999dd
Dumuch <64353295+Dumuch@users.noreply.github.com>
2022-01-31 23:26:53 +0800

    add vuex

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,32 +1,18 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
-// window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
 import "../css/bootstrap.min.css";
-import  "../css/main.css";
+import "../css/main.css";
 import "../css/style.css";
 
 import Vue from "vue";
-import router from "./router";
 
+import router from "./router";
+import store from "./store";
 import App from "./App.vue";
 
 
-
 new Vue({
+    store,
     router,
     render: (h) => h(App),
 }).$mount("#app");
